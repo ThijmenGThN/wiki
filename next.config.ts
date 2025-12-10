837997443bc68d3e06f1acc57086ee1214eff592
ThijmenGThN <mail@thijmenheuvelink.nl>
2025-12-10 23:02:02 +0100

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	reactCompiler: true
+	reactCompiler: true,
+	images: {
+		unoptimized: true
+	}
 }
 
 export default nextConfig
